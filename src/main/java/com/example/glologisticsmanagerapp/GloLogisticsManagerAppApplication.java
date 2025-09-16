4a8e6fe6a3424adc9d643d48cd616cf479de2a94
IacobGiulia <giuliaiacob004@gmail.com>
2025-09-16 20:43:00 +0300

    First commit

---
+++ src/main/java/com/example/glologisticsmanagerapp/GloLogisticsManagerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.glologisticsmanagerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GloLogisticsManagerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GloLogisticsManagerAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/glologisticsmanagerapp/GloLogisticsManagerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.glologisticsmanagerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GloLogisticsManagerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
